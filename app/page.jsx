73830ca0b4cded493f1d36c97eb65a7ac5e4ac17
zazamat <zahirazamat@gmail.com>
2026-03-18 18:24:55 +0400

    Update and rename page.tsx to page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>ValyutaCred</h1>
+      <p>Sayt işləyir.</p>
+    </main>
+  );
+}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>ValyutaCred</h1>
-      <p>Kredit müraciət sistemi qurulur...</p>
-    </main>
-  );
-}
