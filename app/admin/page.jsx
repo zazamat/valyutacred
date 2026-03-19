a391a80424487f1d18779a4f4c11a765462a9709
zazamat <zahirazamat@gmail.com>
2026-03-19 15:45:34 +0400

    Update page.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-"use client";
-
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const cards = [
   {
