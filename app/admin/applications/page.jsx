d024bb15b343be34868c376809c72b4d4a670479
zazamat <zahirazamat@gmail.com>
2026-03-27 13:15:05 +0400

    fix applications client component

--- app/admin/applications/page.jsx
+++ app/admin/applications/page.jsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
 
 import { useCallback, useEffect, useMemo, useState } from "react";
 import Link from "next/link";
